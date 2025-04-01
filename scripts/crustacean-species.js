dfd05cb53d79356d746c68e97b3f13832e2b874d
Kim Charles <hau.kmdeguzman1@gmail.com>
2025-04-01 21:03:58 +0800

    Updated Figures

--- scripts/crustacean-species.js
+++
@@ -1,6 +0,0 @@
-fetch("crustacean-species.html")
-.then(response => response.text())
-.then(data => {
-  document.getElementById("crustacean-container").innerHTML = data;
-})
-.catch(error => console.error("Error loading the HTML file:", error));
